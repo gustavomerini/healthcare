4ecfe65a752f5e4f4e86169197095b29bc3dd49e
merini <gutomerini@gmail.com>
2019-03-13 23:04:34 -0300

    Health Module
    Schedule Component, Meals Component, Workouts Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-import { AuthModule } from "src/auth/auth.module";
 import { Store } from "src/store";
 import { AppComponent } from "./containers/app/app.component";
 import { AppHeaderComponent } from "./components/app-header/app-header.component";
 import { AppNavComponent } from "./components/app-nav/app-nav.component";
 import { AppRoutingModule } from "./app-routing.module";
 
+import { HealthModule } from "src/health/health.module";
+import { AuthModule } from "src/auth/auth.module";
+
 @NgModule({
   declarations: [AppComponent, AppHeaderComponent, AppNavComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, HealthModule],
   providers: [Store],
   bootstrap: [AppComponent]
 })
